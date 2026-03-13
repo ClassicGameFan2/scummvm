b2e2707fcbbdfb7fc0add4558e09335323c1d23d
ClassicGameFan2 <johnleroy55@tutamail.com>
2026-03-13 03:58:45 +0300

    Update actor.cpp replace heigh and width with logical values for upscaling

--- engines/asylum/resources/actor.cpp
+++ engines/asylum/resources/actor.cpp
@@ -3615,7 +3615,7 @@ void Actor::setVisible(bool value) {
 }
 
 bool Actor::isOnScreen() {
-	Common::Rect scene(getWorld()->xLeft, getWorld()->yTop, getWorld()->xLeft + ASYLUM_SCREEN_WIDTH, getWorld()->yTop + ASYLUM_SCREEN_HEIGHT);
+	Common::Rect scene(getWorld()->xLeft, getWorld()->yTop, getWorld()->xLeft + LOGICAL_WIDTH, getWorld()->yTop + LOGICAL_HEIGHT);
 	Common::Rect actor(_boundingRect);
 	actor.translate(_point1.x, _point1.y);
 
